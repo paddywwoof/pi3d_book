d436d8b49ceca4effc3e2c1e53c809e66d9b0805
paddywwoof <patrick@eldwick.org.uk>
2015-11-10 19:58:18 +0000

    fix for model name change

--- programs/demo.py
+++ programs/demo.py
@@ -1,3 +1,5 @@
-import sys
-sys.path.insert(1, '/home/patrick/pi3d')
-
+#import sys
+#sys.path.insert(1, '/home/patrick/pi3d')
+""" uncommenting the above lines will make the example programs use the
+version of pi3d in /home/pi/pi3d
+"""
